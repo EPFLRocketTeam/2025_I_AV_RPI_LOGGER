713cc318f96ff940c7bfa5f11c2f8d312433817e
farah elsousy <farahelsousy@farahs-MacBook-Air.local>
2025-08-06 14:01:08 +0200

    edit

---  logger.cpp
+++  logger.cpp
@@ -4,17 +4,12 @@
 #include <sstream>
 #include <string>
 #include <ctime>
-#include <csignal>
 #include <fcntl.h>
 #include <termios.h>
 #include <unistd.h>
-#include "capsule.h"  // same as your Teensy version, but without Arduino dependencies
+#include "capsule.h"
 
-volatile bool keepRunning = true;
-
-void signalHandler(int signum) {
-    keepRunning = false;
-}
+std::ofstream csv;
 
 std::string getDateStr() {
     time_t t = time(nullptr);
@@ -30,51 +25,70 @@ std::string getDateStr() {
 
 std::string getTimestamp() {
     auto t = std::time(nullptr);
-    auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(
-        std::chrono::system_clock::now().time_since_epoch()) % 1000;
+    struct tm tm;
+    localtime_r(&t, &tm);
 
-    std::ostringstream oss;
-    oss << std::put_time(std::localtime(&t), "%H:%M:%S")
-        << "." << std::setw(3) << std::setfill('0') << ms.count();
-    return oss.str();
+    char buffer[20];
+    strftime(buffer, sizeof(buffer), "%H:%M:%S", &tm);
+
+    return std::string(buffer);
 }
 
-// Packet callback
 void handlePacket(uint8_t packetId, uint8_t *data, uint32_t len) {
-    static std::ofstream csv;
-
     if (!csv.is_open()) {
-        // Generate unique file name
         int counter = 1;
         std::string base = getDateStr() + "_Flight_Data_";
         std::string filename;
-        do {
+        while (true) {
             std::ostringstream oss;
             oss << base << std::setw(2) << std::setfill('0') << counter << ".csv";
             filename = oss.str();
             std::ifstream f(filename);
             if (!f.good()) break;
             counter++;
-        } while (true);
-
+        }
         csv.open(filename);
         std::cout << "Logging to: " << filename << std::endl;
+
+        // Write CSV header
         csv << "timestamp_rpi,packet_id";
-        for (uint32_t i = 0; i < len; i++) csv << ",byte" << i;
+        for (uint32_t i = 0; i < len; i++) {
+            csv << ",byte" << i;
+        }
         csv << "\n";
     }
 
     csv << getTimestamp() << "," << (int)packetId;
-    for (uint32_t i = 0; i < len; i++) csv << "," << (int)data[i];
+    for (uint32_t i = 0; i < len; i++) {
+        csv << "," << (int)data[i];
+    }
     csv << "\n";
-    csv.flush();
+    csv.flush(); // flush after each line to save data immediately
 }
 
-int main() {
-    signal(SIGINT, signalHandler);
+int main(int argc, char* argv[]) {
+    const char* port = "/dev/serial0"; // default UART port
+    int baudrate = B115200;            // default baudrate
 
-    // Open UART
-    int fd = open("/dev/serial0", O_RDWR | O_NOCTTY);
+    if (argc >= 2) {
+        port = argv[1];
+    }
+
+    if (argc >= 3) {
+        int b = atoi(argv[2]);
+        switch (b) {
+            case 9600: baudrate = B9600; break;
+            case 19200: baudrate = B19200; break;
+            case 38400: baudrate = B38400; break;
+            case 57600: baudrate = B57600; break;
+            case 115200: baudrate = B115200; break;
+            default:
+                std::cerr << "Unsupported baudrate, using 115200\n";
+                baudrate = B115200;
+        }
+    }
+
+    int fd = open(port, O_RDWR | O_NOCTTY);
     if (fd < 0) {
         perror("open");
         return 1;
@@ -82,8 +96,8 @@ int main() {
 
     termios tty{};
     tcgetattr(fd, &tty);
-    cfsetispeed(&tty, B115200);
-    cfsetospeed(&tty, B115200);
+    cfsetispeed(&tty, baudrate);
+    cfsetospeed(&tty, baudrate);
     tty.c_cflag |= (CLOCAL | CREAD);
     tty.c_cflag &= ~CSIZE;
     tty.c_cflag |= CS8;
@@ -98,12 +112,20 @@ int main() {
     CapsuleStatic capsule(handlePacket);
 
     uint8_t buf[1];
-    while (keepRunning) {
-        if (read(fd, buf, 1) > 0) {
+    while (true) {
+        ssize_t n = read(fd, buf, 1);
+        if (n > 0) {
             capsule.decode(buf[0]);
+        } else if (n < 0) {
+            perror("read");
+            break; // exit on read error
         }
+        // else n==0 means no data; just continue reading
     }
 
+    if (csv.is_open()) {
+        csv.close();
+    }
     close(fd);
     return 0;
 }
