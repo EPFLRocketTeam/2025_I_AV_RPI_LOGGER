3cf89f01d3c79617507ec6b1305273a8f73da820
Tim Vial <53138133+TimVial@users.noreply.github.com>
2025-11-30 16:06:04 +0100

    auto

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 #include <termios.h>
 #include <stdint.h>
 #include <string.h>
+#include <chrono>
+
 
 #include "../lib/logger/logger.h"
 
@@ -17,9 +19,16 @@ int main()
 
     SerialLogger logger(SERIAL_PORT, baud, filename);
 
+    
+
     while (true)
     {
+        auto start = std::chrono::high_resolution_clock::now();
         logger.poll();
+        auto end = std::chrono::high_resolution_clock::now();
+        auto us = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+        std::cout << "logger.poll() took " << us << " us (" << std::fixed << std::setprecision(3)
+                  << (us / 1000.0) << " ms)\n";
     }
 
     return 0;
